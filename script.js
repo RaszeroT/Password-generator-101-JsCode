52423e5e0f1a0e17a3a57c22cd6fe01fbb939d55
Raszero <travisjcelli95@gmail.com>
2023-09-30 16:47:59 -0500

    added variables for lowercase, uppercase letters and special characters

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
  // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
-
-  
 }
+
+var myChars = ['!','@','#','$','%','^','&','*'];
+var lowercaseLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numbers = ['1','2','3','4','5','6','7','8','9'];
+var uppercaseLetter = lowercaseLetter.map(function(letter){return letter.toUpperCase()});
 
 // Write password to the #password input
 function writePassword() {
